94f270ee9e3a5b9a76f6f7ed8db852fd7841ca28
XNS-ivy <ayramusic.nightcore@gmail.com>
2024-12-05 11:43:47 +0700

    first connect to discord

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-import {} from 'discord.js'
+import pkg from 'discord.js'
+const { Client, GatewayIntentBits } = pkg
+import 'dotenv/config'
 
+const hoshino = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+    ],
+})
+
+hoshino.once('ready', async () => {
+    console.log('Hoshino is ready')
+})
+hoshino.login(process.env.token).catch((err) => { console.log(err) })
